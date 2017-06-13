f6c3f1b4883a940bc36c4a895bd12660c430be10
Eduardo Orive <edorka@gmail.com>
2017-06-13 22:49:18 +0200

    added missing dependencies

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,14 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
 import { AppComponent } from './app.component';
+import { RouterModule, Routes } from '@angular/router';
+import { MaterialModule } from '@angular/material';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    MaterialModule,
     BrowserModule,
     FormsModule,
     HttpModule
