8239d69e41c355b8b756c12416cd38f89037f71a
Eduardo Orive <edorka@gmail.com>
2017-07-19 00:04:26 +0200

    added a button to save current state

--- client/src/app/categories/categories.component.ts
+++ client/src/app/categories/categories.component.ts
@@ -9,18 +9,26 @@ import { Category } from './shared/category'
 })
 export class CategoriesComponent implements OnInit {
   categoriesList: Category[];
+  canSave: Boolean;
   constructor(
     private _categoriesService: CategoriesService
   ) {
     this._categoriesService = _categoriesService;
     this.categoriesList = [];
   }
-
+  save(){
+    this._categoriesService.save(this.categoriesList);
+  }
   ngOnInit() {
     //console.log('new subscription', this.categoriesList);
     this._categoriesService.getList()
         .subscribe(categories => {
           this.categoriesList = categories;
+        });
+    this._categoriesService.getModified()
+        .subscribe(modified => {
+          this.canSave = modified === true;
+          console.log('can save', this.canSave);
         });
   }
 
--- client/src/app/categories/categories.service.ts
+++ client/src/app/categories/categories.service.ts
@@ -8,11 +8,9 @@ import { LocalStorageService } from 'angular-2-local-storage';
 export class CategoriesService {
   list: Category[];
   private changes = new ReplaySubject<Category[]>();
-
+  private modified = new ReplaySubject<Boolean>();
   constructor(
-
     private localStorageService: LocalStorageService
-
   ) {
     var data:any = this.localStorageService.get('categories');
     if ( data === null ){
@@ -22,12 +20,17 @@ export class CategoriesService {
         this.list = parsed_data;
     }
     this.changes.subscribe(list => this.save(list));
+    this.modified.next(false);
     this.changes.next(this.list);
   }
 
   save(list: Category[]){
     console.log('saving')
     this.localStorageService.set('categories', JSON.stringify(list));
+    this.modified.next(false);
+  }
+  getModified(): Observable<Boolean>{
+    return this.modified.asObservable();
   }
   getList(): Observable<Category[]> {
     return this.changes.asObservable();
@@ -36,6 +39,7 @@ export class CategoriesService {
     let position:number = this.list.length;
     this.list.push(category);
     this.changes.next(this.list);
+    this.modified.next(true);
     return position;
   }
   get(position: number): Category{
