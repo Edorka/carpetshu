50b554c478a1f031f4c45c99219f6b00c47e4483
Eduardo Orive <edorka@gmail.com>
2017-08-10 00:03:28 +0200

    basic transit to performance use case

--- client/src/app/categories/performances/performances.component.ts
+++ client/src/app/categories/performances/performances.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { CategoriesService } from '../categories.service'
+import { Category } from '../shared/category'
+import { Competitor } from '../shared/competitor'
 
 @Component({
   selector: 'app-performances',
@@ -6,10 +10,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./performances.component.css']
 })
 export class PerformancesComponent implements OnInit {
-
-  constructor() { }
-
+  id: number;
+  category: Category;
+  private sub: any;
+  constructor(private route: ActivatedRoute, private router: Router, private categories: CategoriesService) { }
   ngOnInit() {
+    this.sub = this.route.params.subscribe(params => {
+       this.id = +params['position']; // (+) converts string 'id' to a number
+       this.category = this.categories.get(this.id);
+       if (this.category === undefined){
+          this.router.navigate(['categories'])
+       }
+    });
+  }
+  add(competitor: Competitor){
+    console.log('adding ',competitor, 'to', this.category);
+    this.category.addCompetitor(competitor);
+    this.categories.setModified();
+  }
+  ngOnDestroy() {
+    this.sub.unsubscribe();
   }
 
 }
