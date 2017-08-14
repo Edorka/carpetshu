af555d78f53d548238c667119adcc1a5c31a6e77
Eduardo Orive <edorka@gmail.com>
2017-08-14 23:51:52 +0200

    retrieval of competitor and category on component show

--- client/src/app/categories/competitors/performance/performance.component.ts
+++ client/src/app/categories/competitors/performance/performance.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Observable } from 'rxjs/Rx';
+import { Category } from '../../shared/category'
+import { CategoryParameters } from '../../shared/category-parameters'
+import { Competitor } from '../../shared/competitor'
+import { CategoriesService } from '../../categories.service'
 
 @Component({
   selector: 'app-performance',
@@ -7,10 +12,26 @@ import { Observable } from 'rxjs/Rx';
   styleUrls: ['./performance.component.css']
 })
 export class PerformanceComponent implements OnInit {
+  categoryId: number;
+  competitorId: number;
+  category: Category;
+  parameters: CategoryParameters;
+  competitor: Competitor;
+  private sub: any;
   timer: Observable<number>;
-  constructor() { }
 
+  constructor(private route: ActivatedRoute, private router: Router, private categories: CategoriesService) { }
   ngOnInit() {
+    this.sub = this.route.params.subscribe(params => {
+       this.categoryId = +params['position']; // (+) converts string 'id' to a number
+       this.category = this.categories.get(this.categoryId);
+       this.parameters = this.category.parameters;
+       this.competitorId = +params['competitorId'];
+       this.competitor = this.category.competitors[this.competitorId];
+       if (this.category === undefined || this.competitor === undefined){
+          this.router.navigate(['categories'])
+       }
+    });
   }
   start(){
     var timer = Observable.timer(0, 1000);
